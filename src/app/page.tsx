8ab0dfe77745c67bef53c90428f66e0293df0d4b
SpidySamurai <javierchiortiz@gmail.com>
2025-07-01 01:17:46 -0600

    Funny salute

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <>
-      <Header/>
+    <main>
+      <Header />
       <h1 className="mt-6 text-3xl text-center text-white">
         Página en construcción
-      </h1>
-
-    </>
+      </h1 >
+      <p className="text-lg text-center text-white mb-6">
+        Tomate un tecito o un cafecito, que ya está por llover 🌧️☕
+      </p>
+      <div className="flex justify-center ">
+        <img
+          src="https://i.gifer.com/7plP.gif" // puedes cambiar este gif por otro si quieres
+          alt="Lluvia tranquila"
+          className="w-72 h-auto rounded-lg shadow-md"
+        />
+      </div>
+    </main>
   );
 }
